83581440b4e853dd77154b2cc8c9579324a38371
= <martyofmca@gmail.coms>
2023-01-16 19:10:05 +0000

    feat: add calculate cube
    add the calculate cube featue to the project. User provides an integer
    and we determine the cube for the number provided.
    
    Resolves: #3

--- Program.cs
+++ Program.cs
@@ -4,3 +4,8 @@ number = float.Parse(Console.ReadLine());
 float _double = number + number;
 Console.WriteLine(_double);
 Console.WriteLine();
+
+// TODO: calculate the cube for the given number
+float cube = number * number * number;
+Console.WriteLine(cube);
+Console.WriteLine();
